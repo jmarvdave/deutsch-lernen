5e2ba1a11aa8a58ba7012813936015b14f2f2c9d
John-Marvin Davis <johndavis@team.mobile.de>
2018-04-14 17:31:21 +0200

    First major refactoring; added more tests

--- src/main/java/application/configuration/ApplicationConfiguration.java
+++ src/main/java/application/configuration/ApplicationConfiguration.java
@@ -1,8 +1,8 @@
 package application.configuration;
 
 import application.service.DeutschLernenService;
-import application.utility.LatexWriter;
-import com.google.api.services.sheets.v4.Sheets;
+import application.writer.LatexWriter;
+import application.transformer.ResponseToSheetTransformer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,17 +10,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ApplicationConfiguration {
 
-    private final Sheets service;
+    private final ResponseToSheetTransformer transformer;
     private final LatexWriter latexWriter;
 
     @Autowired
-    public ApplicationConfiguration(Sheets service, LatexWriter latexWriter) {
-        this.service = service;
+    public ApplicationConfiguration(ResponseToSheetTransformer transformer, LatexWriter latexWriter) {
+        this.transformer = transformer;
         this.latexWriter = latexWriter;
     }
 
     @Bean
     public DeutschLernenService application() {
-        return new DeutschLernenService(service, latexWriter);
+        return new DeutschLernenService(transformer, latexWriter);
     }
 }
---
+++ src/main/java/application/utility/SheetsRetriever.java
@@ -0,0 +1,35 @@
+package application.utility;
+
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.ValueRange;
+
+import java.io.IOException;
+import java.util.List;
+
+public class SheetsRetriever {
+
+    private final Sheets sheetsService;
+
+    public SheetsRetriever(Sheets sheetsService) {
+        this.sheetsService = sheetsService;
+    }
+
+    public List<List<Object>> getValuesFromSheet(String sheetId) {
+        List<List<Object>> values;
+        try {
+            values = getResponseFromService(sheetId);
+        } catch (IOException e) {
+            throw new RuntimeException("Critical failure retrieving data from Google Sheets:" + e);
+        }
+        return values;
+    }
+
+    private List<List<Object>> getResponseFromService(String sheetId) throws IOException {
+        String range = "A2:E";
+        ValueRange response = sheetsService.spreadsheets().values()
+                .get(sheetId, range)
+                .execute();
+
+        return response.getValues();
+    }
+}
