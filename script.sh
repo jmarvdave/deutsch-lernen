8db8e2bbc2e5db4433e421a7230e09cc65d079a4
John-Marvin Davis <davisjohnmarvin@gmail.com>
2018-04-21 19:30:51 +0200

    Editing the script

--- script.sh
+++ script.sh
@@ -4,5 +4,6 @@ cp serviceclient.json target/;
 cd target/;
 java -jar deutsch-lernen-2.0.0.RELEASE-spring-boot.jar;
 pdflatex -interaction=batchmode testing.tex;
+lpr -P davisBrother_DCP-J562DW_d80f9925b8d6 testing.pdf;
 echo "done";
 
