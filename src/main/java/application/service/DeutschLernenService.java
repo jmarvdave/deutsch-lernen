03ad4edce96bcf6c43e0157b225afcb942b76a52
John-Marvin Davis <johndavis@team.mobile.de>
2018-08-17 15:43:02 +0200

    This introduces adjectives as a new type

--- src/main/java/application/service/DeutschLernenService.java
+++ src/main/java/application/service/DeutschLernenService.java
@@ -16,9 +16,11 @@ public class DeutschLernenService {
   private final LatexWriter writer;
   private static final String VERB_SHEET_ID = "1MpUyZq-UDfvHAt2jZN45cEQnyeSk6Nr56_slyQUMcDI";
   private static final String NOUN_SHEET_ID = "1-JL2h2SrndIbNkOGJmueRxcZwqaSl90meGc6fkeJ-Go";
+  private static final String ADJECTIVES_SHEET_ID = "1m32NmkZMzUKgRuhTlBoP5I3Q_BOZ8pL5ci9oFIozWzk";
 
   private static final int NUMBER_OF_VERBS = 14;
-  private static final int NUMBER_OF_NOUNS = 6;
+  private static final int NUMBER_OF_NOUNS = 5;
+  private static final int NUMBER_OF_ADJECTIVES = 1;
 
   public DeutschLernenService(ResponseToSheetTransformer transformer,
       LatexWriter writer,
@@ -32,16 +34,20 @@ public class DeutschLernenService {
   public void run() {
     List<Row> verbRows = transformer.transformToVerbSheet(VERB_SHEET_ID);
     List<Row> nounRows = transformer.transformToNounSheet(NOUN_SHEET_ID);
+    List<Row> adjectiveRows = transformer.transformToAdjectiveSheet((ADJECTIVES_SHEET_ID));
 
     List<Row> verbRowsWithAppliedAlgorithm = algorithmExecutor.apply(verbRows);
     List<Row> nounRowsWithAppliedAlgorithm = algorithmExecutor.apply(nounRows);
+    List<Row> adjectiveRowsWithAppliedAlgorithm = algorithmExecutor.apply(adjectiveRows);
 
     Integer numberOfVerbs = setResultLimit(verbRows, NUMBER_OF_VERBS);
     Integer numberOfNouns = setResultLimit(nounRows, NUMBER_OF_NOUNS);
+    Integer numberOfAdjectives = setResultLimit(nounRows, NUMBER_OF_ADJECTIVES);
 
-    Test test = new TestGenerator(verbRowsWithAppliedAlgorithm, nounRowsWithAppliedAlgorithm)
-        .generate(numberOfVerbs, numberOfNouns);
-    writer.writeTestToFile(test.getRandomVerbs(), test.getRandomNouns(), "testing.tex");
+    Test test = new TestGenerator(verbRowsWithAppliedAlgorithm, nounRowsWithAppliedAlgorithm,
+        adjectiveRowsWithAppliedAlgorithm)
+        .generate(numberOfVerbs, numberOfNouns, numberOfAdjectives);
+    writer.writeTestToFile(test.getRandomVerbs(), test.getRandomNouns(), test.getRandomAdjectives(), "testing.tex");
   }
 
   private int setResultLimit(List<Row> rows, int desiredLimit) {
--- src/test/java/application/generator/TestGeneratorTest.java
+++ src/test/java/application/generator/TestGeneratorTest.java
@@ -1,5 +1,7 @@
 package application.generator;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import application.model.Row;
 import application.model.Row.TYPE;
 import com.google.common.collect.ImmutableList;
@@ -7,8 +9,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestGeneratorTest {
@@ -19,18 +19,21 @@ public class TestGeneratorTest {
     public void setUp() {
         Row verbRow1 = new Row("machen", TYPE.VERB);
         Row verbRow2 = new Row("lachen", TYPE.VERB);
+        Row adjectiveRow1 = new Row("lachen", TYPE.ADJECTIVE);
+        Row adjectiveRow2 = new Row("lachen", TYPE.ADJECTIVE);
         Row nounRow1 = new Row("Laptop", TYPE.NOUN);
         Row nounRow2 = new Row("Gabel", TYPE.NOUN);
         Row nounRow3 = new Row("Messer", TYPE.NOUN);
 
         ImmutableList<Row> verbRows = ImmutableList.of(verbRow1, verbRow2, verbRow2);
         ImmutableList<Row> nounRows = ImmutableList.of(nounRow1, nounRow2, nounRow3, nounRow3, nounRow3);
-        testGenerator = new TestGenerator(verbRows, nounRows);
+        ImmutableList<Row> adjectiveRows = ImmutableList.of(adjectiveRow1, adjectiveRow2);
+        testGenerator = new TestGenerator(verbRows, nounRows, adjectiveRows);
     }
 
     @Test
     public void testGenerate() {
-        application.model.Test generatedTest = testGenerator.generate(2, 3);
+        application.model.Test generatedTest = testGenerator.generate(2, 3, 2);
 
         assertThat(generatedTest.getRandomVerbs().size()).isEqualTo(2);
         assertThat(generatedTest.getRandomNouns().size()).isEqualTo(3);
